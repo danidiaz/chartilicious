88e33176f96d5dd010e97f7106e490796490f812
Daniel Diaz <diaz.carrete@facebook.com>
2021-04-17 13:06:16 +0200

    first commit

---
+++ src/main/java/org/danisoft/chartilicious/ChartiliciousApplication.java
@@ -0,0 +1,13 @@
+package org.danisoft.chartilicious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartiliciousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartiliciousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/danisoft/chartilicious/ChartiliciousApplicationTests.java
@@ -0,0 +1,13 @@
+package org.danisoft.chartilicious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartiliciousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
